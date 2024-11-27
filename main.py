7fe351f627d87aacad92e5c46259931515c65ab5
Gabriel Cassola <cassola.gabriel@gmail.com>
2024-11-27 12:35:36 -0300

    Deploy

--- main.py
+++ main.py
@@ -15,18 +15,6 @@ df = df[df['Android Ver'] != 'NaN']
 df = df[df['Installs'] != 'Free']
 df = df[df['Installs'] != 'Paid']
 
-df['Size'] = df['Size'].apply(lambda x: str(x).replace('Varies with device', 'NaN') if 'Varies with device' in str(x) else x)
-
-df['Size'] = df['Size'].apply(lambda x: str(x).replace('M', '') if 'M' in str(x) else x)
-df['Size'] = df['Size'].apply(lambda x: str(x).replace(',', '') if 'M' in str(x) else x)
-df['Size'] = df['Size'].apply(lambda x: float(str(x).replace('k', '')) / 1000 if 'k' in str(x) else x)
-
-
-df['Size'] = df['Size'].apply(lambda x: float(x))
-df['Installs'] = df['Installs'].apply(lambda x: float(x))
-
-df['Price'] = df['Price'].apply(lambda x: str(x).replace('$', '') if '$' in str(x) else str(x))
-df['Price'] = df['Price'].apply(lambda x: float(x))
 
 
 # Amostra
