ec401dbdd7b1201581648659936f7d02ef508a72
Gabriel Cassola <cassola.gabriel@gmail.com>
2024-11-27 12:37:26 -0300

    Ajuste nos tamanhos

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ df = df[df['Android Ver'] != 'NaN']
 df = df[df['Installs'] != 'Free']
 df = df[df['Installs'] != 'Paid']
 
+# Convertendo tudo para MB
+df['Size'] = df['Size'].apply(lambda x: str(x).replace('Varies with device', 'NaN') if 'Varies with device' in str(x) else x)
+df['Size'] = df['Size'].apply(lambda x: str(x).replace('M', '') if 'M' in str(x) else x)
+df['Size'] = df['Size'].apply(lambda x: str(x).replace(',', '') if 'M' in str(x) else x)
+df['Size'] = df['Size'].apply(lambda x: float(str(x).replace('k', '')) / 1000 if 'k' in str(x) else x)
+df['Size'] = df['Size'].apply(lambda x: float(x))
 
 
 # Amostra
